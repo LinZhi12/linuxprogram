4e7aa5ea10611dc727083ca87b098cd7587c7cfe
LinZhi12 <2224071918@qq.com>
2019-11-05 00:12:16 -0800

    week9 work

---
+++ week9/code/caller2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+  int ref;
+  printf("caller1:pid=%d,ppid=%d\n",getpid(),getppid());
+  execl("/home/rlk/reposi/test1","./test1","123","456",NULL);
+  printf("after calling!\n");
+  sleep(1);
+  return 0;
+}
---
+++ week9/code/system1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc,const char *argv[])
+{
+    int ref=1;
+    if(argc)
+}
