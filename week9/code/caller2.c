1995b580a85d87f3e3a96beeebf60cb5423e69e3
LinZhi12 <2224071918@qq.com>
2019-11-10 04:59:43 -0800

    add week9 work

--- week9/code/caller2.c
+++ week9/code/caller2.c
@@ -4,7 +4,7 @@ int main()
 {
   int ref;
   printf("caller1:pid=%d,ppid=%d\n",getpid(),getppid());
-  execl("/home/rlk/reposi/test1","./test1","123","456",NULL);
+  execl("/home/rlk/reposi/week9/code/test1","./test1","123","456",NULL);
   printf("after calling!\n");
   sleep(1);
   return 0;
